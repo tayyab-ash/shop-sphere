f1ad768252163d75c56926b6b8332294167a2474
Tayyab <mtayab.ash@gmail.com>
2025-01-16 00:25:43 +0500

    useMemo

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,27 +1,43 @@
-import React, { useEffect, useReducer , useRef, useState} from 'react'
+import React, { useEffect, useReducer , useRef, useMemo, useState} from 'react'
 
+
+
+const computeFactorial = (n) => {
+  if (n <= 1) return 1;
+  return n * computeFactorial(n - 1);
+  
+};
+
+ 
+const ExpensiveComputation = ({ num, count }) => {
+  console.log('Component rendered');
+
+  // Factorial will be computed only 1 time
+  // const factorial = useMemo(() => {
+  //   console.log('Computing factorial...');
+  //   return computeFactorial(num)
+  // }, [num]);
+
+  // Factorial will be computed every time the component renders
+  const factorial = () => {
+    console.log('Computing factorial...');
+    return computeFactorial(num);
+  }
+
+  // use f"actorial()" instead of "factorial" in the line below when you want to compute factorial every time the component renders, i.e without useMemo.
+  return <div>Factorial of {num} is {factorial()}</div>;
+};
 
 
 function Footer() {
-  const [first, setfirst] = useState(0)
-  let a = 0;
-  let b = useRef(0);
-  let btnRef =useRef()
-
-  function handleClick() {
-    setfirst(first + 1);
-    
-  }
-  useEffect(() => {
-    console.log(`Rendering.... ${a++} ... and ${b.current++}`)
-    btnRef.current.style.backgroundColor = 'red'
-  })
+  const [count, setCount] = useState(1);  
   
 
   return (
     <div>
-      <span>{first}</span>
-        <button ref={btnRef} onClick={handleClick}>Counter</button>
+      <ExpensiveComputation num={5} count={count} />
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <span>{count}</span>
     </div>
   )
 }
