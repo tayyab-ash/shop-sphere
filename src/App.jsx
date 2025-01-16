0571f7f599181dc20f4312347e99e45c8280c1bb
Tayyab <mtayab.ash@gmail.com>
2025-01-16 19:44:11 +0500

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react';
 import './App.css'
 import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header'
@@ -9,16 +10,25 @@ import {
 
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [word, setWord] = useState("good")
+
+  // const getWord = () => { 
+  //   return "another" + count; 
+  // } 
+
+  const getWord = useCallback(() => { 
+    return "another" + count; 
+  } ,[count]) 
+
   return (
     <>
     <Router>
         <Header/>
-        <Footer/>
-      <Routes>
-      </Routes>
-        
+        <Footer word={"good"} getWord={getWord} />
     </Router>
-    
+    <button className='mr-4' onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <span>{count}</span>
     </>
   )
 }
--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,45 +1,13 @@
-import React, { useEffect, useReducer , useRef, useMemo, useState} from 'react'
+import React, { useEffect, useReducer , useRef, useMemo, memo, useState} from 'react'
 
-
-
-const computeFactorial = (n) => {
-  if (n <= 1) return 1;
-  return n * computeFactorial(n - 1);
-  
-};
-
- 
-const ExpensiveComputation = ({ num, count }) => {
-  console.log('Component rendered');
-
-  // Factorial will be computed only 1 time
-  // const factorial = useMemo(() => {
-  //   console.log('Computing factorial...');
-  //   return computeFactorial(num)
-  // }, [num]);
-
-  // Factorial will be computed every time the component renders
-  const factorial = () => {
-    console.log('Computing factorial...');
-    return computeFactorial(num);
-  }
-
-  // use f"actorial()" instead of "factorial" in the line below when you want to compute factorial every time the component renders, i.e without useMemo.
-  return <div>Factorial of {num} is {factorial()}</div>;
-};
-
-
-function Footer() {
-  const [count, setCount] = useState(1);  
-  
-
+function Footer({word, getWord}) { 
+  console.log("footer is rendered")
   return (
     <div>
-      <ExpensiveComputation num={5} count={count} />
-      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
-      <span>{count}</span>
+      <span>word is {word}</span>
+      <button className='ml-4 bg-slate-500' onClick={()=> getWord()}>{getWord()}</button>
     </div>
   )
 }
 
-export default Footer
+export default memo(Footer)
